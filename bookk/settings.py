1c022dc30ba95f6b7cb0bb2aba7b5f5a948b2d01
Dannysesi <daniel.olusesi20@gmail.com>
2023-11-11 21:57:41 +0100

    settings security update

--- bookk/settings.py
+++ bookk/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'leinadthegr8@gmail.com'
-EMAIL_HOST_PASSWORD = '321456daniel'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
