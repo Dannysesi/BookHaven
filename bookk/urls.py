4e0c3766e5db4931eb90f140106ef6d97e533cdd
Dannysesi <daniel.olusesi20@gmail.com>
2023-11-11 21:57:54 +0100

    added new url route

--- bookk/urls.py
+++ bookk/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
         name='password_reset'),
     path('password-reset/done/',
         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
-        name='password_reset'),
+        name='password_reset_done'),
     path('password-reset-comfirm/<uidb64>/<token>/',
         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
         name='password_reset_confirm'),
