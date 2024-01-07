57b97c86ea96c71e44d9bd242d16a141cb27e7ec
Dannysesi <daniel.olusesi20@gmail.com>
2024-01-07 23:03:02 +0100

    google module error

--- bookk/settings.py
+++ bookk/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 #import boto3
-from google.oauth2 import service_account
+# from google.oauth2 import service_account
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
