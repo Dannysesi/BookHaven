5e0374b595b1fc7150e1bcc34c16ab1b87c81681
Dannysesi <daniel.olusesi20@gmail.com>
2024-02-07 22:35:07 +0100

    trial 2.0

--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 from PIL import Image
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    image = CloudinaryField('image', default='default.png', folder='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -13,9 +14,9 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save()
 
-        img = Image.open(self.image.path)
+        # img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        # if img.height > 300 or img.width > 300:
+        #     output_size = (300,300)
+        #     img.thumbnail(output_size)
+        #     img.save(self.image.path)
