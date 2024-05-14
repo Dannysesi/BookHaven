5d661c67b57757f1c375084ef0543b76588304a8
Dannysesi <daniel.olusesi20@gmail.com>
2024-05-14 23:59:27 +0100

    sharp

--- users/models.py
+++ users/models.py
@@ -14,9 +14,9 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save()
 
-        # img = Image.open(self.image.path)
+        img = Image.open(self.image.path)
 
-        # if img.height > 300 or img.width > 300:
-        #     output_size = (300,300)
-        #     img.thumbnail(output_size)
-        #     img.save(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
